187e634d9fbeee85bea0ce144d49e9761f2d8b9c
Olivier Wertz <olivier.wertz@soprabanking.com>
2022-09-08 15:42:14 +0200

    Modify the input.sql with test query

--- sql_file/input.sql
+++ sql_file/input.sql
@@ -1,15 +1,1 @@
-SELECT 
-	country.country_name_eng,
-	SUM(CASE WHEN call.id IS NOT NULL THEN 1 ELSE 0 END) AS calls,
-	AVG(ISNULL(DATEDIFF(SECOND, call.start_time, call.end_time),0)) AS avg_difference
-FROM country 
-LEFT JOIN city ON city.country_id = country.id
-LEFT JOIN customer ON city.id = customer.city_id
-LEFT JOIN call ON call.customer_id = customer.id
-GROUP BY 
-	country.id,
-	country.country_name_eng
-HAVING AVG(ISNULL(DATEDIFF(SECOND, call.start_time, call.end_time),0)) > (SELECT AVG(DATEDIFF(SECOND, call.start_time, call.end_time)) FROM call)
-ORDER BY calls DESC, country.id ASC;
-
-select p01.typtie "TYPE", count(*) from pyd01 p01 where exists (select null from pyd20 p20 where p20.numtie=p01.numtie and p20.cattie='01') group by p01.typtie order by 2 desc;
+select numtie from (select x from pyd01);
